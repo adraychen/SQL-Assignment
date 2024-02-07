13d3ff74797a98ffc1603a60c9095ba94e4b666e
adraychen <68781407+adraychen@users.noreply.github.com>
2024-02-07 09:28:04 -0500

    Add files via upload

---
+++ orders.sql
@@ -0,0 +1,7 @@
+INSERT INTO Orders (OrderID, OrderDate, TableNumber, ItemID, Quantity, TotalPrice)
+VALUES
+    (1, '2023-12-01', 3, 1, 2, 19.98),
+    (2, '2023-12-02', 5, 3, 1, 11.99),
+    (3, '2023-12-03', 2, 2, 3, 23.97),
+    (4, '2023-12-04', 1, 4, 2, 11.98),
+    (5, '2023-12-05', 4, 5, 1, 8.99)
