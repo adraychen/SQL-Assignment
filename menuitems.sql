95046c8b15d05632464df36a8f8cf794ea201890
adraychen <68781407+adraychen@users.noreply.github.com>
2024-02-07 16:21:29 -0500

    Add files via upload

---
+++ menuitems.sql
@@ -0,0 +1,7 @@
+INSERT INTO MenuItems (ItemID, ItemName, Category, Price, Cost)
+VALUES
+    (1, 'Cheeseburger', 'Main', 9.99, 3.50),
+    (2, 'Caesar Salad', 'Starter', 7.99, 2.50),
+    (3, 'Margherita Pizza', 'Main', 11.99, 4.50),
+    (4, 'Chocolate Cake', 'Dessert', 5.99, 2.00),
+    (5, 'Mojito', 'Drink', 8.99, 1.50);
