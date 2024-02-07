6c4eac0656d8a624c9902ace95db03de1958dc28
adraychen <68781407+adraychen@users.noreply.github.com>
2024-02-07 09:29:00 -0500

    Add files via upload

---
+++ employees.sql
@@ -0,0 +1,7 @@
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Position, HourlyRate)
+VALUES
+    (1, 'John', 'Smith', 'Waiter', 10.50),
+    (2, 'Emma', 'Johnson', 'Chef', 12.75),
+    (3, 'Michael', 'Williams', 'Bartender', 11.25),
+    (4, 'Emily', 'Jones', 'Manager', 15.00),
+    (5, 'David', 'Brown', 'Busser', 9.00)
